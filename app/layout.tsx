b6aac2b114ad358759fe1b73860ae4101a48ed4e
Bridge Trust <thebridgetrustcio@gmail.com>
2026-09-11 14:30:22 +0100

    Replace Google tags and add GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,19 +46,43 @@ export default function RootLayout({
     >
       <body className="relative min-h-screen bg-[var(--page-bg)] text-[var(--foreground)] antialiased">
 
-        {/* Google Analytics */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KGLZNLVF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KGLZNLVF');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
+        {/* Google tag (gtag.js) */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-3E9W643KXM"
-          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-T74XZTKT6V"
+          strategy="beforeInteractive"
         />
 
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="beforeInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'G-3E9W643KXM');
+            gtag('config', 'G-T74XZTKT6V');
           `}
         </Script>
 
